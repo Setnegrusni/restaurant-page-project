0f32944900e2a04daef2b48173fe725e580b6730
Kenneth <krgutgar@gmail.com>
2025-12-15 16:37:20 -0600

    Added logic for module to work

--- src/about.js
+++ src/about.js
@@ -1,5 +1,14 @@
+const mainContent = document.querySelector("#content");
+
 function showAbout() {
-    console.log("Módulo: Acerca de");
+    //console.log("Módulo: Acerca de");
+    const textArea = document.createElement("div");
+    const textAbout = document.createElement("p");
+
+    textAbout.textContent = 'Esta página forma parte del proyecto "Restaurant Page" de "The Odin Project"';
+    textArea.classList.add("text-area");
+    textArea.appendChild(textAbout);
+    mainContent.appendChild(textArea);
 }
 
 export { showAbout };
