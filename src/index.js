3d872e7bba8084ada11ca63ac39d9ac10c98144b
Kenneth <krgutgar@gmail.com>
2025-12-15 16:23:42 -0600

    Added logic for tab change

--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
+//Imports
 import "./styles.css";
-import {showHome} from "./home.js";
+import { showHome } from "./home.js";
+import { showMenu } from "./menu.js";
+import { showAbout } from "./about.js";
 
-console.log("Prueba de funcionamiento");
+//Global variables
+const tabButtons = document.querySelectorAll("button");
+
+//console.log("Prueba de funcionamiento");
+//Carga inicial de la página
 showHome();
+
+//Buttons event listeners
+tabButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        changeTab(button.id);
+    });
+});
+
+//Functions
+
+//Function that changes content based on button clicked
+function changeTab(myTab) {
+    clearContent();
+    
+    switch(myTab) {
+        case "btHome":
+            showHome();
+            break;
+        case "btMenu":
+            showMenu();
+            break;
+        case "btAbout":
+            showAbout();
+            break;
+    }
+}
+
+//Function that clears content
+function clearContent() {
+    const mainContent = document.querySelector("#content");
+    mainContent.innerHTML = "";
+}
