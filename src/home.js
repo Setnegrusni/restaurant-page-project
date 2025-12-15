42412ba228873ad29cfc3d42b054737a4ecb1671
Kenneth <krgutgar@gmail.com>
2025-12-15 16:23:09 -0600

    Added logic for module to work

--- src/home.js
+++ src/home.js
@@ -1,7 +1,24 @@
+import homeImg from "./img/home.png";
 
+const mainContent = document.querySelector("#content");
 
 function showHome() {
-    console.log("Funciona este archivo");
+    //console.log("Funciona este archivo");
+    const homeText = document.createElement("div");
+    const homeBanner = document.createElement("div");
+    const welcomeText = document.createElement("p");
+    const setImg = document.createElement("img");
+
+    homeText.classList.add("home-text");
+    homeBanner.classList.add("home-banner");
+    welcomeText.textContent = "Come and try one of the best cuisines in the world!";
+    setImg.src = homeImg;
+    setImg.classList.add("home-img");
+
+    mainContent.appendChild(homeText);
+    homeText.appendChild(welcomeText);
+    mainContent.appendChild(homeBanner);
+    homeBanner.appendChild(setImg);
 }
 
 export { showHome };
